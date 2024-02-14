5c767080eb686f84c581dd8d18395e06ecbdb817
Adrian <752397@alu.murciaeduca.es>
2024-02-14 13:13:06 +0100

    Creamos las clases sala y reunion y se las importamos al archivo __init__.py.

--- complementos/reuniones/__manifest__.py
+++ complementos/reuniones/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Planificación',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -28,5 +28,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
