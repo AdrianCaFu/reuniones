943f265909ec7d353ee47f6effb0449965c491c1
Adrian <752397@alu.murciaeduca.es>
2024-02-16 14:08:35 +0100

    Añadimos las vistas reuniones y salas. Modificamos el archivo manifest incluyendo esto y la licencia

--- complementos/reuniones/__manifest__.py
+++ complementos/reuniones/__manifest__.py
@@ -12,24 +12,22 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Planificación',
-    'version': '0.2',
+    'category': 'Uncategorized',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/reunion_view.xml',
+        'views/sala_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-}
 
+    'license':  'LGPL-3'
+}
